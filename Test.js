f901673cd554b30f1b26f393161a0573ab85e794
Bohdan Hrudetskyi <razor5556@gmail.com>
2021-11-03 17:05:05 +0200

    first commit

---
+++ Test.js
@@ -0,0 +1,24 @@
+const MATH_PI = 3.14;
+const radius = 10;
+
+let result;
+
+result = radius !== 0 
+? MATH_PI * (radius * radius)
+: "Radius can not be equal to zero";
+
+//console.log(result);
+
+const userName = "Mark";
+
+const discount = userName === "Tony"
+? '30%'
+:'0%';
+
+//console.log("Congrats! Your discount is " + discount);
+
+const res = radius !== 0 && MATH_PI === 3.14
+? MATH_PI * (radius * radius)
+: "Radius can not be equal to zero and MATH_PI should be correct";
+const checkRadius = radius === 10 || radius <100 ? 'Ok' : 'Not Ok';
+console.log(checkRadius);
