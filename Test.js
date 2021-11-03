85dbfc5acdc788b0aee9a874e1bdfea22342b186
Bohdan Hrudetskyi <razor5556@gmail.com>
2021-11-03 17:27:35 +0200

    second commit

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-const MATH_PI = 3.14;
+/*const MATH_PI = 3.14;
 const radius = 10;
 
 let result;
@@ -22,3 +22,19 @@ const res = radius !== 0 && MATH_PI === 3.14
 : "Radius can not be equal to zero and MATH_PI should be correct";
 const checkRadius = radius === 10 || radius <100 ? 'Ok' : 'Not Ok';
 console.log(checkRadius);
+*/
+
+const a = 15;
+const b = 20;
+const sum = a + b;
+
+console.log (a + b);
+
+const constA = 15;
+const constB = 5;
+const result = constA * constB * constB;
+
+//shorter way
+let result1 = 5 * 5;
+result1 *= constB;
+console.log(result1); 
