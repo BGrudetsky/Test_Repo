70012ad770c058a5b3865f56de0a1a8534043e23
Bohdan Hrudetskyi <razor5556@gmail.com>
2021-11-04 11:38:56 +0200

    new commit

--- Test.js
+++ Test.js
@@ -1,26 +1,50 @@
+//types of variables
+let second = 2;
+let SECOND = 2;
+
+let $third = 2;
+let text = "string text";
+
+let PI = 3.14;
+let summ = second + SECOND + $third;
+console.log(summ);
+let decrease = summ - $third;
+console.log(decrease);
+
+
+
+
+
+
+
 //increase value
-const a = 15;
+const a = 15; //types of variables
 const b = 20;
 const sum = a + b;
 console.log (a + b);
+
 //shorter way
-let c = a;
+let c = a; //types of variables
 c += b;
 console.log(c);
+
 //another example
 const constA = 15;
 const constB = 5;
 const result = constA * constB * constB;
 console.log(result);
+
 //shorter way
 let result1 = constB * constB;
 result1 *= constA;
 console.log(result1); 
+
 //decrease value
 let d = 12;
 let e = 7;
 d = d - e;
 console.log(d);
+
 //shorter way
 let newLetter = 12;
 newLetter -= e;
