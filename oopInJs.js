7bc9f9aa13ae4342b9ade108d4f4a888f120a55c
Bohdan Hrudetskyi <bohdan.hrudetskyi@globallogic.com>
2021-11-22 15:40:51 +0200

    end of home work

--- oopInJs.js
+++ oopInJs.js
@@ -1,3 +1,4 @@
+//use classes
 class Animal {
     constructor(name, breed, color) {
         this.name = name;
@@ -60,3 +61,24 @@ class Dog extends Animal {
   const Horsie = new Horse('Horsie', 'running', 'black')
   console.log(Horsie.color);
   
+//use function constructor
+  function Look(hat, sunglasses, dress, gloves, pants, shoes) {
+    this.hat = hat;
+    this.sunglasses = sunglasses;
+    this.dress = dress;
+    this.gloves = gloves;
+    this.pants = pants;
+    this.shoes = shoes;
+   
+    this.winter = function () {
+      console.log(`Look is ${this.hat}, ${this.gloves} gloves , ${this.pants} pants and ${this.shoes}`);
+    };
+    this.summer = function () {
+      console.log(`Look is ${this.hat}, ${this.sunglasses} sunglasses, ${this.dress} dress and ${this.shoes}`);
+    };
+  }
+   
+  const Summerie = new Look("cap", "aviator", "pink", 'white', "jeans", 'boots');
+  const Winterie = new Look("hat", "aviator", "black", "big black", "jeans", "winter boots");
+Winterie.winter();
+Summerie.summer();
